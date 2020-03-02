875d7bfdc85f3cd0441271c8d16c97b9772270d2
xiang3 <ZX814768>
2020-03-02 12:05:04 +0800

    refacter: 重构常量，删除冗余代码

--- src/main/java/com/gildedrose/GildedRose.java
+++ src/main/java/com/gildedrose/GildedRose.java
@@ -5,7 +5,7 @@ import java.util.Arrays;
 class GildedRose {
     private static final String AGED_BRIE = "Aged Brie";
 
-    private static final String BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
+    private static final String BACKSTAGE_PASSES_CONCERT = "Backstage passes to a TAFKAL80ETC concert";
 
     private static final String HAND_OF_RAGNAROS = "Sulfuras, Hand of Ragnaros";
 
@@ -24,13 +24,13 @@ class GildedRose {
     public void update_quality() {
         Arrays.stream(items).forEach( item -> {
             if (!item.name.equals(AGED_BRIE)
-                && !item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
+                && !item.name.equals(BACKSTAGE_PASSES_CONCERT)
                 && item.quality > 0
                 && (!item.name.equals(HAND_OF_RAGNAROS))) {
                     item.quality--;
             }else if(item.quality < QUALITY_FIFTY){
                     item.quality++;
-                if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)
+                if (item.name.equals(BACKSTAGE_PASSES_CONCERT)
                         && (item.sell_in < SELL_IN_ELEVEN && item.quality < QUALITY_FIFTY
                         || item.sell_in < SELL_IN_SIX && item.quality < QUALITY_FIFTY)) {
                     item.quality++;
@@ -43,8 +43,8 @@ class GildedRose {
 
             if (item.sell_in < 0) {
                 if (!item.name.equals(AGED_BRIE)) {
-                    if (item.name.equals(BACKSTAGE_PASSES_TO_A_TAFKAL_80_ETC_CONCERT)) {
-                        item.quality = item.quality - item.quality;
+                    if (item.name.equals(BACKSTAGE_PASSES_CONCERT)) {
+                        item.quality = 0;
                     } else if (item.quality > 0 && !item.name.equals(HAND_OF_RAGNAROS)) {
                         item.quality--;
                     }
